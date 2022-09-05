2f9d7c5b83ff6cdad700021b54a0b7c26b302006
Aslanov Nijad <aslanlinicad@gmail.com>
2022-09-04 22:12:27 -0700

    BeanScope

---
+++ Spring-05-Core-BeanScope/src/main/java/com/spring/MyApp.java
@@ -0,0 +1,25 @@
+package com.spring;
+
+import com.spring.config.ProjectConfig;
+import com.spring.model.Comment;
+import com.spring.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        CommentService commentService = context.getBean(CommentService.class);
+        commentService.publishComment(comment);
+
+
+
+    }
+}
--- Spring-05-Core-BeanScope/src/main/java/com/spring/proxy/EmailCommentNotificationProxy.java
+++ Spring-05-Core-BeanScope/src/main/java/com/spring/proxy/EmailCommentNotificationProxy.java
@@ -2,7 +2,6 @@ package com.spring.proxy;
 
 import com.spring.model.Comment;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
