8efaf1afc5aff8e2f4e2fe7f24895eafe0b4be5e
Aslanov Nijad <aslanlinicad@gmail.com>
2022-09-05 21:05:23 -0700

    spring

--- Spring-05-Core-BeanScope/src/main/java/com/spring/proxy/CommentPushNotificationProxy.java
+++ Spring-05-Core-BeanScope/src/main/java/com/spring/proxy/CommentPushNotificationProxy.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("Push")
+@Qualifier("Ph")
 public class CommentPushNotificationProxy implements CommentNotificationProxy {
     @Override
     public void sendComment(Comment comment) {
