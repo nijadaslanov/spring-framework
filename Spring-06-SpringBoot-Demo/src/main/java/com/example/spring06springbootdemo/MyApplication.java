9bd2ee044349f9b14f4b80eb9e7df3ac3f4259ff
Aslanov Nijad <aslanlinicad@gmail.com>
2022-09-06 08:21:45 -0700

    spring

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/example/spring06springbootdemo/MyApplication.java
@@ -0,0 +1,20 @@
+package com.example.spring06springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06SpringBootDemoApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(Spring06SpringBootDemoApplication.class, args);
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
