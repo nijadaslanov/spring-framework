6bb89f391b2eb7fc646d5446e3f767add0cfc6c6
Aslanov Nijad <aslanlinicad@gmail.com>
2022-09-07 16:46:36 -0700

    mvc

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/example/spring07springmvccontroller/Spring07SpringMvcControllerApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring07springmvccontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//..
+@SpringBootApplication
+public class Spring07SpringMvcControllerApplication {
+/////...
+    public static void main(String[] args) {
+        String str;
+        SpringApplication.run(Spring07SpringMvcControllerApplication.class, args);
+    }
+
+}
