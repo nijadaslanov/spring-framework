bf424a7522fda72faad8ce77b8ee51b8a0f1ca40
Aslanov Nijad <aslanlinicad@gmail.com>
2022-09-07 16:41:04 -0700

    mvc

--- Spring-01-Core-IoC/src/main/java/com/spring/bean_annotation/ConfigAny.java
+++ Spring-01-Core-IoC/src/main/java/com/spring/bean_annotation/ConfigAny.java
@@ -2,7 +2,7 @@ package com.spring.bean_annotation;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//
 @Configuration
 public class ConfigAny {
 
