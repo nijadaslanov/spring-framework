5ee3b038645c277b07d49eff7ddffd27777cb41f
Aslanov Nijad <aslanlinicad@gmail.com>
2022-09-08 13:55:46 -0700

    Model-MVC

--- Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/StudentController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 public class StudentController {
 
@@ -13,9 +17,18 @@ public class StudentController {
 
         // model methods
         model.addAttribute("name", "Nijad");
-        model.addAttribute("course","MVC");
+        model.addAttribute("course", "MVC");
 
+        // create some random student id(0-1000)
+        int d = new Random().nextInt();
+        model.addAttribute("d", d);
 
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(10);
+        model.addAttribute("numbers", numbers);
         return "student/welcome";
     }
 //
