5fe018303fd93583c59e24baad3fa22459b2899a
Aslanov Nijad <aslanlinicad@gmail.com>
2022-09-08 14:58:27 -0700

    Task

---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/Enums/Gender.java
@@ -0,0 +1,5 @@
+package com.example.Enums;
+
+public enum Gender {
+    MALE, FEMALE;
+}
---
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/controller/MentorController.java
@@ -0,0 +1,32 @@
+package com.example.controller;
+
+import com.example.Enums.Gender;
+import com.example.model.Mentor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+public class MentorController {
+
+    @RequestMapping("/list")
+    public String table(Model model) {
+
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(new Mentor("Mike", "Smith", 45, Gender.MALE));
+        mentorList.add(new Mentor("Tom", "Hanks", 30, Gender.MALE));
+        mentorList.add(new Mentor("Amy", "Bryan", 40, Gender.FEMALE));
+
+        model.addAttribute("mentors", mentorList);
+
+        return "mentor/mentor-list";
+    }
+
+
+}
