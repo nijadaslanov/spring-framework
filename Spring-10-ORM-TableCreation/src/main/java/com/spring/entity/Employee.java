ef4e2827894c03df7e43308fd13955ff033870ba
Aslanov Nijad <aslanlinicad@gmail.com>
2022-10-18 20:47:01 -0700

    h2

---
+++ Spring-10-ORM-TableCreation/src/main/java/com/spring/entity/Employee.java
@@ -0,0 +1,15 @@
+package com.spring.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    private int id;
+    private String name;
+
+
+}
---
+++ Spring-10-ORM-TableCreation/src/main/java/com/spring/entity/Student.java
@@ -0,0 +1,17 @@
+package com.spring.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+
+    @Id
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+}
