a51aa8a8534955cf05552c9e84a082b8661db321
Aslanov Nijad <aslanlinicad@gmail.com>
2022-10-19 22:30:49 -0700

    Mapping

--- Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/spring/bootstrap/DataGenerator.java
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/spring/bootstrap/DataGenerator.java
@@ -1,18 +1,14 @@
 package com.spring.bootstrap;
 
 import com.spring.enums.Status;
-import com.spring.model.Customer;
-import com.spring.model.Merchant;
-import com.spring.model.Payment;
-import com.spring.model.PaymentDetail;
-import com.spring.repository.CustomerRepository;
-import com.spring.repository.MerchantRepository;
-import com.spring.repository.PaymentRepository;
+import com.spring.model.*;
+import com.spring.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Arrays;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
@@ -21,11 +17,16 @@ public class DataGenerator implements CommandLineRunner {
     private final PaymentRepository paymentRepository;
     private final MerchantRepository merchantRepository;
     private final CustomerRepository customerRepository;
+    private final ItemRepository itemRepository;
+    private final CartRepository cartRepository;
 
-    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository, CustomerRepository customerRepository) {
+
+    public DataGenerator(PaymentRepository paymentRepository, MerchantRepository merchantRepository, CustomerRepository customerRepository, ItemRepository itemRepository, CartRepository cartRepository) {
         this.paymentRepository = paymentRepository;
         this.merchantRepository = merchantRepository;
         this.customerRepository = customerRepository;
+        this.itemRepository = itemRepository;
+        this.cartRepository = cartRepository;
     }
 
     @Override
@@ -50,6 +51,26 @@ public class DataGenerator implements CommandLineRunner {
 
         payment1.setMerchant(merchant1);
         payment2.setMerchant(merchant1);
+
+        Item item1 = new Item("Milk", "M01");
+        Item item2 = new Item("Sugar", "M03");
+        Item item3 = new Item("Bread", "B1");
+
+
+        Cart cart1 = new Cart();
+        Cart cart2 = new Cart();
+
+
+        cart1.setItemList(Arrays.asList(item1, item2, item3));
+        cart2.setItemList(Arrays.asList(item1, item2));
+
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        itemRepository.save(item3);
+
+        cartRepository.save(cart1);
+        cartRepository.save(cart2);
 
         customerRepository.save(customer1);
 
--- Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/spring/model/Cart.java
+++ Spring-12-ORM-OneToMany-ManyToMany/src/main/java/com/spring/model/Cart.java
@@ -18,5 +18,6 @@ public class Cart {
 
 
     @ManyToMany
+    @JoinTable(name = "cart_item_rel", joinColumns = @JoinColumn(name = "c_id"), inverseJoinColumns = @JoinColumn(name = "i_id"))
     private List<Item> itemList;
 }
