2f81d455b4def0bf1ee6da627ae7729185c9e34c
Aslanov Nijad <aslanlinicad@gmail.com>
2022-10-20 09:48:42 -0700

    cinemaLab

---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/spring/entity/Movie.java
@@ -0,0 +1,37 @@
+package com.spring.entity;
+
+import com.spring.enums.MovieState;
+import com.spring.enums.MovieType;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Movie extends BaseEntity {
+
+    private String name;
+    @Column(columnDefinition = "DATE")
+    private LocalDate releaseDate;
+    private Integer duration;
+    @Column(columnDefinition = "text")
+    private String summary;
+    @Enumerated(EnumType.STRING)
+    private MovieType movieType;
+    @Enumerated(EnumType.STRING)
+    private MovieState movieState;
+    private BigDecimal price;
+
+    @JoinTable(name = "movie_genre_rel",
+            joinColumns = @JoinColumn(name = "movie_id"),
+            inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    @ManyToMany
+    private List<Genre> genreList;
+
+
+}
---
+++ Spring-13-ORM-CinemaLab/src/main/java/com/spring/enums/MovieType.java
@@ -0,0 +1,12 @@
+package com.spring.enums;
+
+public enum MovieType {
+
+
+
+
+
+
+
+
+}
